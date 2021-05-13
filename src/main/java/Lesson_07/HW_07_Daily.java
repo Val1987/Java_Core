d8e58f818e18eb72b835ab6180b92523d29270e2
val1987 <TigashovVE@gmail.com>
2021-05-13 22:21:26 +0700

    7

---
+++ src/main/java/Lesson_07/HW_07_Daily.java
@@ -0,0 +1,27 @@
+package Lesson_07;
+
+import java.util.Date;
+
+public class HW_07_Daily {
+
+    public long dt;
+    public long sunrise;
+    public long sunset;
+    public long moonrise;
+    public long moonset;
+    public double moon_phase;
+    public HW_07_Temp temp;
+    public HW_07_FeelsLike feels_like;
+    public double pressure;
+    public double humidity;
+    public double dew_point;
+    public double wind_speed;
+    public double wind_deg;
+    public double wind_gust;
+    public HW_07_Weather[] weather;
+    public int clouds;
+    public double pop;
+    public double rain;
+    public double uvi;
+
+}
---
+++ src/main/java/Lesson_07/HW_07_Temp.java
@@ -0,0 +1,12 @@
+package Lesson_07;
+
+public class HW_07_Temp {
+
+    public double day;
+    public double min;
+    public double max;
+    public double night;
+    public double eve;
+    public double morn;
+
+}
